70b0c29dc6064868dcaf6b67c43e14d562c63df7
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2015-08-25 18:06:53 +0200

    alt json reader

--- cpp-grappa/from_json.hpp
+++ cpp-grappa/from_json.hpp
@@ -7,8 +7,7 @@
 
 using namespace Grappa;
 
-PointList from_json() {
-    int NPOINTS = 100000;
+void read_points(GPoint _points, GPoint _centroids, size_t num_points, size_t num_clusters) {
     int i=0;
     json_t *json;
     json_error_t error;
@@ -17,6 +16,52 @@ PointList from_json() {
     json_t *value;
 
     std::cout << "reading points... " ;
+
+    double* xs = new double[num_points];
+    double* ys = new double[num_points];
+
+    json = json_load_file("../points.json", 0, &error);
+    if(!json) {
+        throw std::runtime_error("Error parsing Json file");
+    }
+
+    json_array_foreach(json, index, value) {
+        xs[index] = json_number_value(json_array_get(value,0));
+        ys[index] = json_number_value(json_array_get(value,1));
+    }
+
+    std::cout << "done " << std::endl;
+
+    
+    std::cout << "writing to GlobalMem... " ;
+
+
+    for(int i = 0; i < num_points; i++){
+        Point p(xs[i],ys[i]);
+        delegate::write(_points+i,p);
+        if (i < num_clusters) {
+            delegate::write(_centroids+i, p);
+        }
+    };
+
+    std::cout << "done" << std::endl;
+
+}
+
+
+
+GlobalAddress<Point> from_json(size_t NPOINTS) {
+    int i=0;
+    json_t *json;
+    json_error_t error;
+    size_t index;
+    long int temp = 0;
+    json_t *value;
+
+    std::cout << "reading points... " ;
+
+
+    GlobalAddress<Point> points = global_alloc<Point>(NPOINTS);
 
     double* xs = new double[NPOINTS];
     double* ys = new double[NPOINTS];
@@ -35,12 +80,9 @@ PointList from_json() {
 
     std::cout << "done " << std::endl;
 
-    GlobalAddress<Point> points = global_alloc<Point>(NPOINTS);
     
     std::cout << "writing to GlobalMem... " ;
 
-
-    NPOINTS = 100;
 
     for(int i=0; i < NPOINTS; i++){
         Point p(xs[i],ys[i]);
@@ -49,6 +91,6 @@ PointList from_json() {
 
     std::cout << "done" << std::endl;
 
-    return PointList(points, NPOINTS);
+    return points;
 }
 
