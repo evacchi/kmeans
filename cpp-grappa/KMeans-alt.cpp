e79559b44f6387e3e077e6c18aaef066778c2073
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2015-08-26 10:01:49 +0200

    alt version

--- cpp-grappa/KMeans-alt.cpp
+++ cpp-grappa/KMeans-alt.cpp
@@ -11,13 +11,21 @@
 typedef GlobalAddress<int> GInt;
 using namespace Grappa;
 
-/**
- * Wierd name but essential function; decides witch centroid is closer to the given point
- * @param point point given
- * @param centroids pointer to centroids array
- * @param num_centroids number of centroids to check
- * @return closest centroid's index in centroids array(2nd param)
- */
+void reset_centroids(GPoint _points, GPoint _centroids, size_t num_points, size_t num_centroids) {
+    for(int i = 0; i < num_centroids; i++){
+        Point p = delegate::read(_points+i);
+        delegate::write(_centroids+i, p);
+    }
+}
+
+void reset_clusters(GInt _clusters, size_t num_points) {
+    for(int i = 0; i < num_points; i++){
+        delegate::write(_clusters+i, 0);
+    }
+}
+
+
+
 int closest(Point point, Point* centroids, int num_centroids)
 {
     int _closest=0;
@@ -34,22 +42,11 @@ int closest(Point point, Point* centroids, int num_centroids)
     }
     return _closest;
 }
-/**
- * Cumulative function that must be called after the closest centroid for each point is found
- * Calculates new centroids as describen in kmeans algorithm
- * @param points array of points
- * @param data array of cluster assignments
- * @param centroids return array of centroids
- * @param num_clusters number of clusters(so number of centroids)
- * @param num_points number of points 
- */
-
-//          update_centroids(gpoints, gclusters, gcentroids, num_clusters, num_points);
 
 void update_centroids(GPoint points, GInt clusters, GPoint centroids, int num_clusters, int num_points)
 {
 
-    DVLOG(0) << "update_centroids";
+    DVLOG(3) << "update_centroids";
     Point new_centroids[num_clusters];
     int   population[num_clusters];
 
@@ -80,7 +77,7 @@ void update_centroids(GPoint points, GInt clusters, GPoint centroids, int num_cl
 
     };
 
-    DVLOG(0) << "counters updated";
+    DVLOG(3) << "counters updated";
 
     for(int64_t i = 0; i < num_clusters; i++) {
         if(population[i] != 0)
@@ -91,20 +88,25 @@ void update_centroids(GPoint points, GInt clusters, GPoint centroids, int num_cl
     };
 
 
-    DVLOG(0) << "centroids updated";
+    DVLOG(3) << "centroids updated";
 
 
 }
 
 void main_body() {
 
+
+    int repetitions = 2;
     
     int num_clusters = 10;
     int num_points   = 10000;
     int niters = 15;
 
-    int job_size = num_points / Grappa::cores();
 
+    int job_size = num_points / Grappa::cores();
+    
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> elapsed_seconds;
         
     Grappa::run([=] {  
 
@@ -112,71 +114,85 @@ void main_body() {
         GPoint gpoints    = global_alloc<Point>(num_points);
         GInt   gclusters  = global_alloc<int>(num_points);
         
-        read_points(gpoints, gcentroids, num_points, num_clusters);
+        read_points(gpoints, num_points);
 
-        DVLOG(0) << "points read. begin.";
-        
-        //Main job of master processor is done here     
-        for(int iter = 0; iter < niters; iter++)
-        {   
-            DVLOG(0) << "iter " << iter;
 
-            on_all_cores([=]{
+        for (int t = 0; t < repetitions; t++) {
+            reset_centroids(gpoints, gcentroids, num_points, num_clusters);
+            reset_clusters(gclusters, num_points);
 
-                int64_t my_offset = job_size * Grappa::mycore();
-                int64_t my_job_size = job_size;
+            start = std::chrono::system_clock::now();
 
-                if (Grappa::mycore() == Grappa::cores() - 1)
-                    my_job_size += num_points % Grappa::cores();
 
-                // double decl to work-around C++ capture behavior w/ array refs
+            DVLOG(3) << "points read. begin.";
+            
+            //Main job of master processor is done here     
+            for(int iter = 0; iter < niters; iter++)
+            {   
+                DVLOG(3) << "iter " << iter;
 
-                Point _points[my_job_size];
-                Point* points = _points;
+                on_all_cores([=]{
+
+                    int64_t my_offset = job_size * Grappa::mycore();
+                    int64_t my_job_size = job_size;
+
+                    if (Grappa::mycore() == Grappa::cores() - 1)
+                        my_job_size += num_points % Grappa::cores();
+
+                    // double decl to work-around C++ capture behavior w/ array refs
+
+                    Point _points[my_job_size];
+                    Point* points = _points;
+                    
+                    Point _centroids[num_clusters];
+                    Point* centroids = _centroids;
+
+                    DVLOG(3) << "fetch centroids " << my_job_size;
+
                 
-                Point _centroids[num_clusters];
-                Point* centroids = _centroids;
+                    // fetch points from gmemory
+                    forall_here(0, num_clusters, [=](int64_t i) {
+                        centroids[i] = delegate::read(gcentroids+i);
+                    });
 
-                DVLOG(0) << "fetch centroids " << my_job_size;
+                    DVLOG(3) << "fetch points " << my_job_size;
 
-            
-                // fetch points from gmemory
-                forall_here(0, num_clusters, [=](int64_t i) {
-                    centroids[i] = delegate::read(gcentroids+i);
+                
+                    forall_here(0, my_job_size, [=](int64_t i) {
+                        points[i] = delegate::read(gpoints + my_offset + i);
+                    });
+
+                    DVLOG(3) << "points were fetched. start";
+
+                    // compute & write back gclusters to gmemory
+                    forall_here(my_offset, my_job_size, [=](int64_t j) {
+                        delegate::write(gclusters + j, my_offset + closest(points[j], centroids, num_clusters));
+                    });
+                    DVLOG(3) << "finished work.";
+
+
                 });
+                
+                update_centroids(gpoints, gclusters, gcentroids, num_clusters, num_points);
 
-                DVLOG(0) << "fetch points " << my_job_size;
+            }
 
-            
-                forall_here(0, my_job_size, [=](int64_t i) {
-                    points[i] = delegate::read(gpoints + my_offset + i);
-                });
-
-                DVLOG(0) << "points were fetched. start";
-
-                // compute & write back gclusters to gmemory
-                forall_here(my_offset, my_job_size, [=](int64_t j) {
-                    delegate::write(gclusters + j, my_offset + closest(points[j], centroids, num_clusters));
-                });
-                DVLOG(0) << "finished work.";
-
-
-            });
-            
-            update_centroids(gpoints, gclusters, gcentroids, num_clusters, num_points);
+            end = std::chrono::system_clock::now();
+            elapsed_seconds += ( end-start );
 
         }
+
+
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+     
+        std::cout << "finished computation at " << std::ctime(&end_time)
+                  << "elapsed time: " << elapsed_seconds.count() / (double) repetitions << "s\n";
+
+
         
     });
 }
 
-/**
- * main function
- * divided to two brances for master & slave processors respectively
- * @param argc commandline argument count
- * @param argv array of commandline arguments
- * @return 0 if success
- */
 int main(int argc, char* argv[])
 {
     Grappa::init(&argc, &argv);
@@ -184,4 +200,3 @@ int main(int argc, char* argv[])
     Grappa::finalize();
     return 0;
 }
-/* EOF */
